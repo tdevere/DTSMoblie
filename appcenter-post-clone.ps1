0b5eefd578211472d65303fbae6697ff8da7225d
tdevere <tdevere@microsoft.com>
2021-12-15 17:31:18 -0600

    fix powershell script

--- appcenter-post-clone.ps1
+++ appcenter-post-clone.ps1
@@ -1,10 +1,14 @@
 write-host "appcenter-post-clone script: Remove Projects from Solution"
 write-host "Searching for Solution Files"
 
-$SLN_PATH = Get-ChildItem -Path $env:APPCENTER_SOURCE_DIRECTORY -Include *.sln 
+#Notes: Include behavior in PowerShell
+#https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.management/get-childitem?view=powershell-7.2#example-4--get-child-items-using-the-include-parameter
+
+
+$SLN_PATH = Get-ChildItem -Path $env:APPCENTER_SOURCE_DIRECTORY -Include *.sln -Recurse
 $AllProjects = Get-ChildItem -Path $env:APPCENTER_SOURCE_DIRECTORY -Include *.csproj -Exclude "*UWP*.csproj", "ServiceRequest.csproj" -Recurse
 write-host $SLN_PATH
-write-host $AllProjects
+
 foreach ($project in $AllProjects)
 {
     write-host "Removing $project from $SLN_PATH"
